cbe57b556b2e2ba8aab6997c9138e2d81b988c97
Josh Richards <josh.t.richards@gmail.com>
2024-07-14 07:49:32 -0400

    feat: add debug logging to sendmail binary finder
    Signed-off-by: Josh Richards <josh.t.richards@gmail.com>

--- lib/private/Mail/Mailer.php
+++ lib/private/Mail/Mailer.php
@@ -302,6 +302,7 @@ class Mailer implements IMailer {
 				if ($sendmail === false) {
 					// fallback (though not sure what good it'll do)
 					$sendmail = '/usr/sbin/sendmail';
+					$this->logger->debug('sendmail binary search failed, using fallback ' . $sendmail, ['app' => 'core']);
 				}
 				$binaryPath = $sendmail;
 				break;
@@ -312,6 +313,7 @@ class Mailer implements IMailer {
 			default => ' -bs',
 		};
 
+		$this->logger->debug('Using sendmail binary: ' . $binaryPath, ['app' => 'core']);
 		return new SendmailTransport($binaryPath . $binaryParam, null, $this->logger);
 	}
 }
